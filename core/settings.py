7b57b438563f12cb46ba1d3a79b58ce8f508e7ba
AdamMatheus <adam.matheus.it@gmail.com>
2022-04-09 16:47:39 +0200

    updated

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,16 +40,19 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'quiz',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'core.urls'
@@ -113,13 +117,32 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS =(
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+]
+
+CORS_ALLOW_ALL_ORIGINS=True
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
--- quiz/views.py
+++ quiz/views.py
@@ -1,22 +1,22 @@
 from rest_framework import generics
 from rest_framework.response import Response
-from .models import Quizzes,Question
-from .serializers import QuizSerializer,RandomQuestionSerializer,QuestionSerializer
+from .models import Quizzes,Question, Answer
+from .serializers import QuizSerializer,RandomQuestionSerializer,AnswerSerializer
 from rest_framework.views import APIView
 
+
+class StartQuiz (APIView):
+    
+    def get(self,request,**kwargs):
+        quiz = Quizzes.objects.filter(category__name=kwargs['title'])
+        serializer=QuizSerializer(quiz,many=True)
+        return Response(serializer.data)
 class Quiz(generics.ListAPIView):
     
     serializer_class = QuizSerializer
     queryset =Quizzes.objects.all()
 
-class RandomQuestion(APIView):
-    
-    def get(self, request, format=None, **kwargs):
-        question = Question.objects.filter(quiz__title=kwargs['topic']).order_by('?')[:1]
-        serializer = RandomQuestionSerializer(question, many=True)
-        return Response(serializer.data)
-    
-class QuizQuestion(APIView):
+class RandomQuestionTopic(APIView):
     
     def get(self, request, format=None, **kwargs):
         question = Question.objects.filter(quiz__title=kwargs['topic']).order_by('?')[:1]
